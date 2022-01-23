f9d147651bba9d67eaa8534fc8c6355116658857
Long Hoang <heroprotunlong@gmail.com>
2022-01-24 01:34:24 +0700

    main mechanics added
    - added tile matching function
    - added drag and drop balls between tiles
    - game manager to handle game states

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public enum GameState
+    {
+        MOVE,
+        MATCH,
+        SPAWN
+    }
+
     [SerializeField]
     private GameObject tileContainer;
 
@@ -13,15 +20,188 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject tilePrefab;
 
+    private GameObject[,] tileArray;
+
+    private GameState currentState;
+    public bool canMove;
+
+    private TileType[] randomSpawnQueue;
+    private Vector2[] randomSpawnPos;
+
+    private int score;
+    private bool gameFinish;
+
     // Start is called before the first frame update
     void Start()
     {
-        float distance_between_tile = 1.25f;
+        int play_area_size = 9;
+        float distance_between_tile = 1f;
+        tileArray = new GameObject[play_area_size, play_area_size];
+
+        // Instantiate tiles 
+        for(int y = 0; y < play_area_size; y++)
+        {
+            for(int x = 0; x < play_area_size; x++)
+            {
+                float offset = distance_between_tile * (float)play_area_size / 2;
+                Vector3 pos = new Vector3(x * distance_between_tile - 4f, -y * distance_between_tile + 4f);
+                GameObject newTile = Instantiate(tilePrefab, Vector3.zero, Quaternion.identity, tileContainer.transform);
+
+                newTile.transform.localPosition = pos;
+                tileArray[x, y] = newTile;
+                Tile newTileScript = newTile.GetComponent<Tile>();
+                newTileScript.Balls = balls;
+                newTileScript.Manager = GetComponent<GameManager>();
+                newTileScript.coord = new Vector2(x, y);
+            }
+        }
+
+        // Connecting tiles
+        for (int y = 0; y < play_area_size; y++)
+        {
+            for (int x = 0; x < play_area_size; x++)
+            {
+                tileArray[x, y].GetComponent<Tile>().ConnectTiles(x, y, tileArray);
+            }
+        }
+
+        currentState = GameState.MOVE;
+        gameFinish = false;
+
+        randomSpawnQueue = new TileType[3];
+        randomSpawnPos = new Vector2[3];
+        RandomSpawnColor();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(GameUpdate());
+    }
+
+    IEnumerator GameUpdate()
+    {
+        while (!gameFinish)
+        {
+            switch (currentState)
+            {
+                case GameState.SPAWN:
+                    #region Spawn random balls
+                    RandomSpawnPosition();
+
+                    for (int i = 0; i < randomSpawnQueue.Length; i++)
+                    {
+                        int x = (int)randomSpawnPos[i].x;
+                        int y = (int)randomSpawnPos[i].y;
+
+                        tileArray[x, y].GetComponent<Tile>().CurrentTile = randomSpawnQueue[i];
+                    }
+
+                    RandomSpawnColor();
+
+                    currentState = GameState.MOVE;
+                    canMove = true;
+                    break;
+                    #endregion
+
+                case GameState.MOVE:
+                    #region Let the player move the balls between tiles
+                    while(canMove)
+                    {
+                        yield return null;
+                    }
+                    currentState = GameState.MATCH;
+                    break;
+                    #endregion
+
+                case GameState.MATCH:
+                    #region Search for matching tiles and increment point based on result
+                    for (int y = 0; y < 9; y++)
+                    {
+                        for (int x = 0; x < 9; x++)
+                        {
+                            Tile currentTile = tileArray[x, y].GetComponent<Tile>();
+                            if (!currentTile.IsCleared && currentTile.CurrentTile != TileType.EMPTY)
+                            {
+                                currentTile.MatchTile(1);
+                            }
+                        }
+                    }
+
+                    for (int y = 0; y < 9; y++)
+                    {
+                        for (int x = 0; x < 9; x++)
+                        {
+                            Tile currentTile = tileArray[x, y].GetComponent<Tile>();
+
+                            // Reset check flag
+                            currentTile.IsChecked = false;
+                            
+                            // Destroy balls and increment score
+                            if (currentTile.IsCleared)
+                            {
+                                score++;
+                                currentTile.CurrentTile = TileType.EMPTY;
+                                currentTile.IsCleared = false;
+                            }
+                        }
+                    }
+
+                    currentState = GameState.SPAWN;
+                    //gameFinish = !gameFinish;
+                    break;
+                    #endregion
+            }
+
+            // End game when all tiles are filled
+            int filled_tile_count = 0;
+
+            for (int y = 0; y < 9; y++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    Tile currentTile = tileArray[x, y].GetComponent<Tile>();
+                    if (currentTile.CurrentTile != TileType.EMPTY)
+                    {
+                        filled_tile_count++;
+                    }
+                }
+            }
+
+            if (filled_tile_count >= 81)
+                gameFinish = true;
+        }
+    }    
+
+    // Randomize colors for next 3 balls
+    private void RandomSpawnColor()
+    {
+        for (int i = 0; i < randomSpawnQueue.Length; i++)
+        {
+            int rand = Random.Range(1, 4);
+
+            randomSpawnQueue[i] = (TileType)rand;
+        }
+    }
+
+    // Randomize positions for next 3 balls
+    private void RandomSpawnPosition()
+    {
+        for(int i = 0; i < randomSpawnQueue.Length; i++)
+        {
+            randomSpawnPos[i] = Vector2.one * -1;
+
+            // Getting random position for random spawn
+            while (randomSpawnPos[i].x < 0 || randomSpawnPos[i].y < 0)
+            {
+                int randomX = Random.Range(0, 8);
+                int randomY = Random.Range(0, 8);
+
+                if (tileArray[randomX, randomY].GetComponent<Tile>().CurrentTile == TileType.EMPTY)
+                {
+                    randomSpawnPos[i] = new Vector2(randomX, randomY);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,38 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileType
+{
+    EMPTY,
+    RED,
+    GREEN,
+    BLUE
+}
+
 public class Tile : MonoBehaviour
 {
-    public enum TileType
-    {
-        EMPTY,
-        RED,
-        GREEN,
-        BLUE
-    }
+    #region Fields
 
     [SerializeField]
     private Sprite[] tileSprites;
     [SerializeField]
     private TileType currentTile;
 
+    // Fields for dragging functions
     private GameObject[] balls;
     private Vector3[] ballOriginalPos;
     private GameObject currentBall;
     private Vector3 currentBallPos;
-
     private SpriteRenderer sr;
-    private bool dragged; 
-    
+    private bool dragged;
+
+    private GameObject[] surroundingTiles;
+    private bool isChecked; // Tag for matching tiles
+    private bool isCleared; // Tag for destroying balls and add score
+
+    private GameManager manager;
+    public Vector2 coord;
+    #endregion
+
     public GameObject[] Balls
     {
         set
         {
+            if(balls == null)
+                balls = new GameObject[value.Length];
+
             for(int i = 0; i < value.Length; i++)
             {
                 balls[i] = value[i];
             }
         }
+    }
+
+    public TileType CurrentTile
+    {
+        get { return currentTile; }
+        set { currentTile = value; }
+    }
+
+    public bool IsChecked
+    {
+        get { return isChecked; }
+        set { isChecked = value; }
+    }
+
+    public bool IsCleared
+    {
+        get { return isCleared; }
+        set { isCleared = value; }
+    }
+
+    public GameManager Manager
+    {
+        set { manager = value; }
+        get { return manager; }
     }
 
     // Start is called before the first frame update
@@ -48,17 +85,20 @@ public class Tile : MonoBehaviour
         {
             ballOriginalPos[i] = balls[i].transform.position;
         }
+
+        isChecked = false;
+        isCleared = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!dragged)
+        if (!dragged && currentTile != TileType.EMPTY)
         {
             currentBall = balls[(int)currentTile - 1];
             currentBallPos = currentBall.transform.position;
-            UpdateVisual();
         }
+        UpdateVisual();
     }
 
     void UpdateVisual()
@@ -66,26 +106,145 @@ public class Tile : MonoBehaviour
         sr.sprite = tileSprites[(int)currentTile];
     }
 
+    public void ConnectTiles(int x, int y, GameObject[,] tileArray)
+    {
+        int arrayMin = 0;
+        int arrayMax = 8;
 
+        surroundingTiles = new GameObject[8];
 
-    // Mouse drag functions for dragging balls
+        for (int i = 0; i < surroundingTiles.Length; i++)
+            surroundingTiles[i] = null;
+
+        if (y > arrayMin)
+            surroundingTiles[0] = tileArray[x, y - 1];
+
+        if (y > arrayMin && x < arrayMax)
+            surroundingTiles[1] = tileArray[x + 1, y - 1];
+
+        if (x < arrayMax)
+            surroundingTiles[2] = tileArray[x + 1, y];
+
+        if (y < arrayMax && x < arrayMax)
+            surroundingTiles[3] = tileArray[x + 1, y + 1];
+
+        if (y < arrayMax)
+            surroundingTiles[4] = tileArray[x, y + 1];
+
+        if (y < arrayMax && x > arrayMin)
+            surroundingTiles[5] = tileArray[x - 1, y + 1];
+
+        if (x > arrayMin)
+            surroundingTiles[6] = tileArray[x - 1, y];
+
+        if (x > arrayMin && y > arrayMin)
+            surroundingTiles[7] = tileArray[x - 1, y - 1];
+    }
+
+    public bool MatchTile(int count, int direction = -1)
+    {
+        bool isMatch = false;
+        bool isComplete = false;
+
+        // Continue from the main direction
+        if(direction > 0 && surroundingTiles[direction] != null)
+        {
+            Tile nextTile = surroundingTiles[direction].GetComponent<Tile>();
+
+            if (nextTile.CurrentTile == currentTile)
+            {
+                isComplete = nextTile.MatchTile(count + 1, direction);
+            }
+            else if(count >= 5)
+            {
+                isCleared = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Check surrounding tiles
+        if (direction < 0)
+        {
+            for (int i = 0; i < surroundingTiles.Length; i++)
+            {
+                GameObject nextTile = null;
+                Tile nextTileScript = null;
+
+                if (surroundingTiles[i] != null && surroundingTiles[i].GetComponent<Tile>().CurrentTile != TileType.EMPTY)
+                {
+                    nextTile = surroundingTiles[i];
+                    nextTileScript = nextTile.GetComponent<Tile>();
+                }
+                else continue;
+
+                if (nextTileScript.CurrentTile == currentTile)
+                {
+                    isMatch = true;
+                    // Else, check all surrounding tiles for a match
+                    if (direction < 0)
+                        isComplete |= nextTileScript.GetComponent<Tile>().MatchTile(count + 1, i);
+                }
+            }
+        }
+
+        if(!isCleared)
+            isCleared = isComplete;
+
+        return isComplete;
+    }
+
+    #region Mouse drag functions for dragging balls
+
     private void OnMouseDown()
     {
-        dragged = true;
-        sr.sprite = tileSprites[0];
+        if (manager.canMove)
+        {
+            dragged = true;
+            sr.sprite = tileSprites[0];
+        }
     }
 
     private void OnMouseDrag()
     {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-        curPosition.z = -5;
-        currentBall.transform.position = curPosition;
+        if (currentTile != TileType.EMPTY && manager.canMove)
+        {
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+            curPosition.z = -5;
+            currentBall.transform.position = curPosition;
+        }
     }
 
     private void OnMouseUp()
     {
-        currentBall.transform.position = currentBallPos;
-        dragged = false;
+        if (manager.canMove)
+        {
+            currentBall.transform.position = currentBallPos;
+            dragged = false;
+
+            RaycastHit2D hit;
+            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+            curPosition.z = 0;
+
+            // CircleCast to get the tile 
+            hit = Physics2D.CircleCast(curPosition, 0.1f, Vector2.zero);
+            Tile newTileScript = hit.collider.gameObject.GetComponent<Tile>();
+
+            // Check if the tile is in the 4 directions of the original tile and is empty
+            if ((coord.x == newTileScript.coord.x || coord.y == newTileScript.coord.y) && newTileScript.CurrentTile == TileType.EMPTY)
+            {
+                newTileScript.CurrentTile = currentTile;
+                currentTile = TileType.EMPTY;
+
+                manager.canMove = false;
+            }
+        }
     }
+
+    #endregion
 }
